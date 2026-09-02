14c9aed1e158e0aefce57e6736aeeb8a0d217c64
Sevastian Bahynskyi <114170185+Sevastian-Bahynskyi@users.noreply.github.com>
2026-09-02 16:47:18 +0200

    feat: show loading during route transitions

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,9 +1,11 @@
 import { AppNav } from './AppNav'
+import { RouteTransitionFeedback } from './RouteTransitionFeedback'
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="app-shell">
       <AppNav />
+      <RouteTransitionFeedback />
       <main className="main-content">{children}</main>
     </div>
   )
